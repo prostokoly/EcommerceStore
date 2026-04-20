78a42e16bcc5f0fc976f5b5c25644101dbae146d
prostokoly <kolasosnov177@gmail.com>
2026-04-20 21:34:30 +0400

    Фильтрация

--- client/src/pages/CatalogPage.jsx
+++ client/src/pages/CatalogPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useCallback } from "react";
+import ProductList from "../component/product/ProductList/ProductList";
+import { useSearchParams } from "react-router-dom";
+import { getProductCategory } from "../services/api";
+import { useFetch } from "../hooks/useFetch";
+import ProductCard from "../component/product/ProductCard/ProductCard";
+import CategoriesSection from "../component/home/CategoriesSection/CategoriesSection";
 
 const CatalogPage = () => {
-    return <div>CatalogPage</div>;
+    const [searchParams] = useSearchParams();
+    const category = searchParams.get("category");
+
+    const fetch = useCallback(() => getProductCategory(category), [category]);
+    const { data, isLoading } = useFetch(fetch);
+    console.log(data);
+
+    if (!data?.productList || data.productList.length === 0) {
+        return <h2>В этой категории товаров пока нет</h2>;
+    }
+    return (
+        <div className="catalog-page">
+            <CategoriesSection />
+            <div>
+                {data.productList.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default CatalogPage;
